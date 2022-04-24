3c6edada3fc7afd7540039e14b4301f8616e67aa
Alexsail8 <alexnik.88kam@gmail.com>
2022-04-24 15:10:10 +0300

    feat: add lazy Loading Images

--- image-search-api/js/index.js
+++ image-search-api/js/index.js
@@ -2,7 +2,8 @@ const containerImgs = document.querySelector('#grid')
 const searchField = document.querySelector('#input')
 const searchBtn = document.querySelector('.search__button')
 const clearBtn = document.querySelector('.clear__button')
-
+let lazyImages = document.querySelectorAll('img[data-src]')
+const windowHeight = document.documentElement.clientHeight
 const data = {
 	searchField: 'fractal',
 }
@@ -34,9 +35,10 @@ async function getData(url) {
 	const res = await fetch(url)
 	const result = await res.json()
 
-	console.log(result)
 	if (result.results.length > 0) {
 		addImgBlock(result.results)
+		lazyImages = document.querySelectorAll('img[data-src]')
+		lazyImagesPushArray()
 	} else {
 		console.log('403 - Too many request')
 	}
@@ -49,9 +51,13 @@ function addImgBlock(arrAPI) {
 	arrAPI.forEach(post => {
 		let urlRegular = post.urls.regular
 		const blockImg = document.createElement('div')
-		blockImg.classList.add('img')
-		blockImg.setAttribute('data-url', `${urlRegular}`)
-		blockImg.style.backgroundImage = `url(${urlRegular})`
+		const imgAPI = document.createElement('img')
+		imgAPI.classList.add('lazyload', 'loading-img')
+		imgAPI.setAttribute('data-src', `${urlRegular}`)
+		imgAPI.setAttribute('src', `./assets/img/loading-default.png`)
+		blockImg.classList.add('img__api')
+
+		blockImg.append(imgAPI)
 		containerImgs.append(blockImg)
 	})
 }
@@ -73,34 +79,71 @@ function loadFromLocalStorage() {
 	}
 }
 
+// lazy Loading Images
+let lazyImagesPositions = []
+function lazyImagesPushArray() {
+	lazyImagesPositions = []
+	if (lazyImages.length > 0) {
+		lazyImages.forEach(img => {
+			if (img.dataset.src) {
+				lazyImagesPositions.push(img.getBoundingClientRect().top + scrollY)
+
+				lazyScrollCheck()
+			}
+		})
+	}
+}
+function lazyScroll() {
+	if (lazyImages.length > 0) {
+		lazyScrollCheck()
+	}
+}
+
+function lazyScrollCheck() {
+	var imgIndex = lazyImagesPositions.findIndex(
+		item => scrollY > item - windowHeight
+	)
+
+	if (imgIndex >= 0) {
+		if (lazyImages[imgIndex].dataset.src) {
+			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src
+			lazyImages[imgIndex].removeAttribute('data-src')
+		}
+		delete lazyImagesPositions[imgIndex]
+	}
+}
+
 // popup change
 function popupImage(elem) {
 	const target = elem.target
-	if (target.className == 'img') {
+	if (target.classList.contains('loading-img')) {
 		tooglePopup(target)
 	}
 }
 
 function tooglePopup(target) {
-	const url = target.dataset.url
+	let url = ''
+	if (target.hasAttribute('src')) {
+		url = target.getAttribute('src')
+	}
 
-	let popupBg = document.querySelector('.popupSC__bg')
-	let popup = document.querySelector('.popupSC')
-	let openPopupButtons = document.querySelectorAll('.open-popupSC')
-	let closePopupButton = document.querySelector('.close-popupSC')
-	let bodyPopupSC = document.querySelector('.body-popupSC')
+	let popupBg = document.querySelector('.popup__bg')
+	let popup = document.querySelector('.popup')
+	let openPopupButtons = document.querySelectorAll('.open-popup')
+	let closePopupButton = document.querySelector('.close-popup')
+	let bodyPopup = document.querySelector('.body-popup')
 
 	popupBg.classList.add('active')
 	popup.classList.add('active')
 
 	if (url) {
-		bodyPopupSC.innerHTML = ''
+		bodyPopup.innerHTML = ''
 		const blockPopup = document.createElement('div')
 		const imgPopup = document.createElement('img')
 		imgPopup.src = url
 		imgPopup.classList.add('imgPopup')
 		blockPopup.append(imgPopup)
-		bodyPopupSC.append(blockPopup)
+		bodyPopup.append(blockPopup)
 	}
 
 	if (closePopupButton) {
@@ -119,7 +162,7 @@ function tooglePopup(target) {
 }
 
 document.addEventListener('click', popupImage.bind(this))
-
+window.addEventListener('scroll', lazyScroll)
 window.addEventListener('DOMContentLoaded', function (e) {
 	loadFromLocalStorage()
 
